bfbc37cf3645633d2771ee21db40a47c45d1fb27
Rodolfo Herrera Hernandez <codingwithrodi@gmail.com>
2021-09-25 17:30:32 -0300

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -1,6 +1,6 @@
 /***
  * Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project root 
+ * Licensed under the MIT license. See LICENSE file in the project root
  * for full license information.
  *
  * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
